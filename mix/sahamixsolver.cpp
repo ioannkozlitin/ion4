cefd7dfaaccdcfe3199eaadd32ac316fb0cc4d1a
kozlitin <kozlitin@elins.ru>
2020-02-26 14:38:44 +0300

    Сузил отрезок поиска xe

--- mix/sahamixsolver.cpp
+++ mix/sahamixsolver.cpp
@@ -9,8 +9,8 @@ double SahaMixSolver::operator()(const MixData &data)
     SahaLeft sahaLeft;
     FindRoot findroot;
 
-    unsigned int maxZ = data.elements[0].Z;
-    for(int i = 1; i < data.x.size(); i++) if(data.elements[i].Z > maxZ) maxZ = data.elements[i].Z;
+    double maxZ = 0;
+    for(int i = 0; i < data.x.size(); i++) maxZ += data.elements[i].Z * data.x[i];
 
     double xe = findroot(-746, log(double(maxZ)), [&](double x) {return sahaLeft(data, data.V, x);}, 1e-7, data.T, data.V);
 
